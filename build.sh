b6adb2d61b53375fcae180024934421ea13a5dd8
dkovacevic <dejankov@gmail.com>
2018-11-03 11:51:40 +0100

    Message format

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 mvn package -DskipTests=true -Dmaven.javadoc.skip=true
-docker build -t dejankovacevic/alert-bot:latest .
-docker push dejankovacevic/alert-bot
-kubectl delete pod -l name=alert -n prod
-kubectl get pods -l name=alert -n prod
-
+docker build -t $DOCKER_USERNAME/alert-bot:latest .
+docker push $DOCKER_USERNAME/alert-bot
+kubectl delete pod -l name=alert
+kubectl get pods -l name=alert
